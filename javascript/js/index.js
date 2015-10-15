68229a3f70bb1bf38e766287945f61bd53d8855f
CoreyCole <coreyleoc@gmail.com>
2015-10-15 15:29:42 -0700

    class work from Tuesday

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,28 @@
 /**
  * application script for index.html
  */
+
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+
+    var i;
+
+    function forEachElement(stuff, funcz) {
+        for (i = 0; i < stuff.length; i++) {
+            funcz(stuff[i]);
+        }
+    }
+
+    var butts = document.getElementById("click-me");
+    butts.addEventListener("click", function() {
+        console.log("ayy lmao");
+    });
+
+    var closeButts = document.querySelectorAll(".alert .close");
+    //cant use foreach becuase query selector all doesn't return iterable array (its a node list)
+    for (i = 0; i < closeButts.length; i++) {
+        closeButts[i].addEventListener("click", function() {
+           this.parentElement.style.display = "none";
+        });
+    }
+});
--- javascript/js/states.js
+++ javascript/js/states.js
@@ -1,3 +1,29 @@
 /**
  * application script for the states.html
  */
+
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+
+    var ul = document.getElementById("states-list");
+
+    function addStates(states, listElem) {
+        listElem.innerHTML = "";
+        states.forEach(function(state) {
+            var li = document.createElement("li");
+            li.textContent = state.name;
+            listElem.appendChild(li);
+        });
+    }
+
+    var statesFilter = document.getElementById("state-filter-field");
+    statesFilter.addEventListener("keyup", function() {
+        var filter = this.value.toLowerCase();
+        var filteredStates = usaStates.filter(function(state) {
+            return state.name.toLowerCase().indexOf(filter) >= 0;
+        });
+        addStates(filteredStates, ul);
+    });
+
+    addStates(usaStates, ul);
+});
