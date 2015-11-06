5ce216dff4a0cb1ee14249800892da16c16f4fc0
CoreyCole <coreyleoc@gmail.com>
2015-11-05 16:58:24 -0800

    firebase and forms

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -10,13 +10,7 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
     .constant('storageKey', 'contacts-list')
     .factory('contacts', function(uuid, localStorageService, storageKey) {
         // factory is a fancy way of fetching data and making it available to controllers
-        return [{
-            id: 'default-delete-me-later',
-            fname: 'wall',
-            lname: 'E',
-            phone: '206-555-1212',
-            dob: '1/1/2050'
-        }];
+        return localStorageService.get(storageKey);
     })
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -39,6 +33,18 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         //default if something weird is typed in
         $urlRouterProvider.otherwise('/contacts');
     })
+    //register a directive for custom validation of dates in the past
+    .directive('inThePast', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) {
+                    var today = new Date();
+                    return (new Date(modelValue) <= today);
+                };
+            }
+        };
+    })
     .controller('ContactsController', function($scope, contacts) {
         //passed in contacts from whatever contacts factory returned
         $scope.contacts = contacts;
@@ -52,7 +58,8 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
             return contact.id === $stateParams.id;
         });
     })
-    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
+    .controller('EditContactController', function($scope, $stateParams, $state,
+                                                  uuid, localStorageService, storageKey, contacts) {
         var existingContact = contacts.find(function(contact) {
             return contact.id === $stateParams.id;
         });
@@ -61,8 +68,16 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $scope.contact = angular.copy(existingContact);
 
         $scope.save = function() {
-            //on save, copy back into actual data
-            angular.copy($scope.contact, existingContact);
+            if ($scope.contact.id) {
+                //on save, copy back into actual data
+                angular.copy($scope.contact, existingContact);
+            } else {
+                $scope.contact.id = uuid.v4();
+                contacts.push($scope.contact);
+            }
+
+            localStorageService.set(storageKey, contacts);
+
             $state.go('list');
         }
     });
--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,24 @@
 
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
+        //create reference to the Firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+
+        //firebase array comes from angular fire library
+        $scope.messages = $firebaseArray(ref);
+
+        //initialize form fields
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessage = function() {
+
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP
+            });
+        }; //sendMessage()
     });
